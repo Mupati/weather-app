00a21630e3f57e6a5bac7e27266a6368dbe04ce2
Mupati <scientificgh@gmail.com>
2021-04-29 16:56:31 +0000

    Wait for data to be fully ready

--- src/components/WeatherChart.js
+++ src/components/WeatherChart.js
@@ -7,15 +7,18 @@ import { selectWeatherData } from "../features/weather/weatherSlice";
 function WeatherChart() {
   const selectedDay = useSelector((state) => state.weather.selectedDay);
   const dayWeatherData = useSelector(selectWeatherData);
+  const isDataReady = Object.keys(dayWeatherData).length !== 0;
 
   const graphData = {
     labels:
-      Object.keys(dayWeatherData).length !== 0 &&
+      isDataReady &&
       dayWeatherData[selectedDay].map((data) => formatWeatherTime(data.dt_txt)),
     datasets: [
       {
         label: `Temperatures on ${formatWeatherDay(selectedDay)}`,
-        data: dayWeatherData[selectedDay].map((data) => data.main.temp),
+        data:
+          isDataReady &&
+          dayWeatherData[selectedDay].map((data) => data.main.temp),
         backgroundColor: "rgba(54, 162, 235, 0.2)",
         borderColor: "rgba(54, 162, 235, 1)",
         borderWidth: 1,
@@ -23,7 +26,7 @@ function WeatherChart() {
     ],
   };
 
-  return <Bar data={graphData} data-testid="weather-chart" />;
+  return isDataReady && <Bar data={graphData} data-testid="weather-chart" />;
 }
 
 export default WeatherChart;
